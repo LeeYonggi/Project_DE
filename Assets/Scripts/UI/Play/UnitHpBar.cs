947becb6b52c1ce265ff3b10b00c7aeb715a4a0f
olivesch <olivesch@naver.com>
2021-04-12 00:05:24 +0900

    클래시로얄 HP 참고 업데이트

--- Assets/Scripts/UI/Play/UnitHpBar.cs
+++ Assets/Scripts/UI/Play/UnitHpBar.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UnitHpBar : MonoBehaviour
 {
+    [SerializeField] Image blendImage = null;
     private Character parentChar = null;
     private Slider slider = null;
     
     private float maxHp = default;
     private float curHp = default;
+
+
+    private Tween blendingHpBarImage = null;
+
     
     public void InitHpBar(Character parent)
     {
@@ -24,5 +30,13 @@ public class UnitHpBar : MonoBehaviour
     public void ChangeCurHp(float changeHp)
     {
         slider.value = curHp = changeHp;
+        blendingHpBarImage?.Kill();
+
+        blendImage.color = new Color(1, 1, 1, 0.4f);
+        blendingHpBarImage = blendImage.DOColor(new Color(1, 1, 1, 0), 0.5f).OnComplete(() => {
+            blendingHpBarImage = null;
+        });
     }
+
+    
 }
